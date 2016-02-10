899fe7540f0a2e7e532879e43ac091a7688b1ad5
AliYousuf <aliyousuf2012@hotmail.com>
2016-02-10 23:35:50 +0400

    file with Cmakefile.txt

---
+++ MyInfo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+void Iam()
+{
+   std::cout << "Name : Ali Yousuf" << std::endl; 
+   std::cout << "Email : aliyousuf2012@hotmail.com" << std::endl;
+   std::cout << " This is firest cmake " << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "MyInfo.h"
+int main(int argc,char** argv)
+{
+    Iam();
+}
